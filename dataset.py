9318dd609bf01072dd6666d41d2074ec1bf63f20
Pannuba <occhipinti99@disroot.org>
2022-07-25 21:02:13 +0200

    separate dataset file

---
+++ dataset.py
@@ -0,0 +1,56 @@
+import pandas
+
+class Dataset:
+
+	def __init__(self, path):
+		self.df = pandas.read_excel(path, engine='openpyxl').drop(columns=['Exp SciExpeM ID', 'Experiment DOI', 'Chem Model', 'Chem Model ID'])
+		self.df = self.df.drop(self.df.columns[0], axis=1)		# Removes the first unnamed column
+		
+		self.calculateAvg('Phi')
+		self.calculateAvg('Pressure (Bar)')		# Also update the main dataset with the average values for phi, P and T
+		self.calculateAvg('Temperature (K)')
+
+		self.columns = {}
+	
+		self.columns['Experiment Type'] = self.df['Experiment Type'].tolist()
+		self.columns['Reactor'] = self.df['Reactor'].tolist()
+		self.columns['Target'] = self.df['Target'].tolist()
+		self.columns['Fuels'] = self.df['Fuels'].tolist()
+
+		self.permutations = self.getPermutations()
+	
+	def calculateAvg(self, column):
+
+		for i in range(self.df.shape[0]):
+			string = self.df[column].iloc[i][1:-1].replace(',', '')		# [1:-1] removes the first and last char, so '(1.0, 1.0)' becomes '1.0, 1.0'. replace() removes the comma
+			minmax = [float(num) for num in string.split()]  			# List with the min and max value of the interval
+			self.df[column].iloc[i] = (minmax[0] + minmax[1]) / 2		# Updates the cell with the average
+		
+		self.df[column] = pandas.to_numeric(self.df[column])
+	
+	def createDictList(self):
+		expTypeDict = createDict(self.df, 'Experiment Type')	# So I keep the dictionary to analyze the results in each cluster (to reconvert from number to string)
+		reactorDict = createDict(self.df, 'Reactor')
+		targetDict = createDict(self.df, 'Target')
+		fuelsDict = createDict(self.df, 'Fuels')
+
+		self.dictList = [expTypeDict, reactorDict, targetDict, fuelsDict]
+
+	def getPermutations(self):			# Returns the list of permutations (dict list) given a list of columns. Generic approach to getPermutations
+		permutationsList = []
+		columnNames = list(self.columns)
+		rows = len(self.columns[columnNames[0]])
+
+		for i in range(rows):		# For each row
+			
+			tempDict = {}
+
+			for col in self.columns:		# For each column (col = key of columns dict = column name). columns = {ColName : [colValues], ...}
+			
+				tempDict[col] = self.columns[col][i]	# Add cell values to dict
+
+			if tempDict not in permutationsList:
+				permutationsList.append(tempDict)
+
+		return permutationsList		# List of all possible permutations in the dataset (by categoric columns)
+
--- project.py
+++ project.py
@@ -1,8 +1,9 @@
-import openpyxl, pandas, sklearn, itertools, scipy.stats
+import openpyxl, sklearn, itertools, scipy.stats
 from sklearn.cluster import KMeans
 from pandas.api.types import is_numeric_dtype
 from pathlib import Path
 from plotting import *
+from dataset import *
 
 #pandas.set_option('display.max_rows', None)
 
@@ -270,61 +271,6 @@ def getCorrelationMatrix(dataset):	# Builds a matrix where every cell is the cor
 		corrMatrix.loc[len(corrMatrix.index)] = newRow		# Appends the new row to the correlation matrix
 
 	return corrMatrix
-
-
-class Dataset:
-
-	def __init__(self, path):
-		self.df = pandas.read_excel(path, engine='openpyxl').drop(columns=['Exp SciExpeM ID', 'Experiment DOI', 'Chem Model', 'Chem Model ID'])
-		self.df = self.df.drop(self.df.columns[0], axis=1)		# Removes the first unnamed column
-		
-		self.calculateAvg('Phi')
-		self.calculateAvg('Pressure (Bar)')		# Also update the main dataset with the average values for phi, P and T
-		self.calculateAvg('Temperature (K)')
-
-		self.columns = {}
-	
-		self.columns['Experiment Type'] = self.df['Experiment Type'].tolist()
-		self.columns['Reactor'] = self.df['Reactor'].tolist()
-		self.columns['Target'] = self.df['Target'].tolist()
-		self.columns['Fuels'] = self.df['Fuels'].tolist()
-
-		self.permutations = self.getPermutations()
-	
-	def calculateAvg(self, column):
-
-		for i in range(self.df.shape[0]):
-			string = self.df[column].iloc[i][1:-1].replace(',', '')		# [1:-1] removes the first and last char, so '(1.0, 1.0)' becomes '1.0, 1.0'. replace() removes the comma
-			minmax = [float(num) for num in string.split()]  			# List with the min and max value of the interval
-			self.df[column].iloc[i] = (minmax[0] + minmax[1]) / 2		# Updates the cell with the average
-		
-		self.df[column] = pandas.to_numeric(self.df[column])
-	
-	def createDictList(self):
-		expTypeDict = createDict(self.df, 'Experiment Type')	# So I keep the dictionary to analyze the results in each cluster (to reconvert from number to string)
-		reactorDict = createDict(self.df, 'Reactor')
-		targetDict = createDict(self.df, 'Target')
-		fuelsDict = createDict(self.df, 'Fuels')
-
-		self.dictList = [expTypeDict, reactorDict, targetDict, fuelsDict]
-
-	def getPermutations(self):			# Returns the list of permutations (dict list) given a list of columns. Generic approach to getPermutations
-		permutationsList = []
-		columnNames = list(self.columns)
-		rows = len(self.columns[columnNames[0]])
-
-		for i in range(rows):		# For each row
-			
-			tempDict = {}
-
-			for col in self.columns:		# For each column (col = key of columns dict = column name). columns = {ColName : [colValues], ...}
-			
-				tempDict[col] = self.columns[col][i]	# Add cell values to dict
-
-			if tempDict not in permutationsList:
-				permutationsList.append(tempDict)
-
-		return permutationsList		# List of all possible permutations in the dataset (by categoric columns)
 
 
 def main():
